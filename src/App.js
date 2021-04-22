b11a5953510785424affd6c287ae1332e18b80b6
juliRicomagno <Julian@JR-GI.tpzargentina.com.ar>
2021-04-22 10:32:29 -0300

    updated Movie

--- src/App.js
+++ src/App.js
@@ -15,25 +15,34 @@ const App = () => {
 	const getMovieRequest = async (searchValue) => {
 		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
 
+
 		const response = await fetch(url);
 		const responseJson = await response.json();
 
 		if (responseJson.Search) {
 			setMovies(responseJson.Search);
+		}else{
+			const url2 = `http://www.omdbapi.com/?s=all&apikey=263d22d8`;
+			const response2 = await fetch(url2);
+			const responseJson2 = await response2.json();
+			setMovies(responseJson2.Search);
 		}
 	};
 
 	const addFavouriteMovie = (movie) => {
 		const newFavouriteList = [...favourites, movie];
+		
 		setFavourites(newFavouriteList);
+		
 	};
 
 	const removeFavouriteMovie = (movie) => {
 		const newFavouriteList = favourites.filter(
 			(favourite) => favourite.imdbID !== movie.imdbID
 		);
-
+		
 		setFavourites(newFavouriteList);
+		
 	};
 
 	useEffect(() => {
